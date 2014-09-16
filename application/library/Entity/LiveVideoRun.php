8c8b06b85ffa40db58df44e9b29e020ee1182712
julius xue <yingang@thenetcircle.com>
2014-09-16 17:16:59 +0800

    add missed entity

--- application/library/Entity/LiveVideoRun.php
+++ application/library/Entity/LiveVideoRun.php
@@ -1,2 +1,193 @@
 <?php
-/*a memory table same structure with live_video, (inheritence)*/
+namespace Entity;
+/**
+ * @Entity
+ * @Table(name="live_video_run", options={"collate"="utf8_general_ci", "engine"="memory"})
+ */
+class LiveVideoRun
+{
+		/**
+     * @Id @Column(type="integer")
+     */
+    private $id;
+    /** @Column(length=255) */
+    private $title;
+    /** @Column(length=255) */
+    private $img;
+    /** @Column(length=50) */
+    private $source;
+    /** @Column(length=255) */
+    private $vurl;
+    /** @Column(length=20) */
+    private $game;
+    /** @Column(type="datetime", name="created_at") */
+    private $createdAt;
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     *
+     * @return LiveVideoRun
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return LiveVideoRun
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set img
+     *
+     * @param string $img
+     *
+     * @return LiveVideoRun
+     */
+    public function setImg($img)
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    /**
+     * Get img
+     *
+     * @return string
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * Set source
+     *
+     * @param string $source
+     *
+     * @return LiveVideoRun
+     */
+    public function setSource($source)
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    /**
+     * Get source
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return $this->source;
+    }
+
+    /**
+     * Set vurl
+     *
+     * @param string $vurl
+     *
+     * @return LiveVideoRun
+     */
+    public function setVurl($vurl)
+    {
+        $this->vurl = $vurl;
+
+        return $this;
+    }
+
+    /**
+     * Get vurl
+     *
+     * @return string
+     */
+    public function getVurl()
+    {
+        return $this->vurl;
+    }
+
+    /**
+     * Set game
+     *
+     * @param string $game
+     *
+     * @return LiveVideoRun
+     */
+    public function setGame($game)
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+
+    /**
+     * Get game
+     *
+     * @return string
+     */
+    public function getGame()
+    {
+        return $this->game;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return LiveVideoRun
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
