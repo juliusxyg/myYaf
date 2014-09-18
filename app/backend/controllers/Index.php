7f4acd54bc734f68f96de29a50c7b9cb0ae59581
julius xue <yingang@thenetcircle.com>
2014-09-18 13:07:29 +0800

    add bootstrap3 as style

--- app/backend/controllers/Index.php
+++ app/backend/controllers/Index.php
@@ -11,9 +11,22 @@ class IndexController extends Yaf\Controller_Abstract
 
   public function loginAction() 
   {//默认Action
-   	
+   	if($this->getRequest()->isPost())
+    {
+      $username = $this->getRequest()->getPost("username","");
+      $password = $this->getRequest()->getPost("password","");
+      if($username == Yaf\Registry::get("config")->get("admin")->name && 
+         $password == Yaf\Registry::get("config")->get("admin")->passwd)
+      {
+        Yaf\Session::getInstance()->set("Authentication", true);
+      }else{
+        Yaf\Session::getInstance()->set("Authentication", false);
+      }
+    }
+
     $this->getView()->assign("content", "I am backend login");
     Yaf\Registry::get("layout")->setTemplate("layoutGuest.pthml");
+    
   }
 }
 ?>
